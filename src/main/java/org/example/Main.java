6ee5302d4f4531e92d0c909efbe943df05f282fc
dreik84 <dreik84@mail.ru>
2025-12-26 10:19:00 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String currencyRate = Converter.getCurrencyByDate("02/03/2002");
+        System.out.println(currencyRate);
     }
 }
