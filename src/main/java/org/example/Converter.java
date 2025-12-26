957faf486aeb43f9afbec0be23cbde04fa80a2e6
dreik84 <dreik84@mail.ru>
2025-12-26 10:19:35 +0300

    Update Converter.java

--- src/main/java/org/example/Converter.java
+++ src/main/java/org/example/Converter.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Converter {
+
+    public static String getCurrencyByDate(String date) throws IOException, InterruptedException {
+        URI uri = URI.create("http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date);
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body().toString();
+    }
 }
