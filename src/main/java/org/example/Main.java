835b809b883d099f955fc95686500aea2f5c1dfa
dreik84 <dreik84@mail.ru>
2025-12-27 10:14:50 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String currencyRate = Converter.getCurrencyByDate("02/03/2002");
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String currencyRate = Converter.getCurrencyByDate(formatter.format(today));
         System.out.println(currencyRate);
     }
 }
