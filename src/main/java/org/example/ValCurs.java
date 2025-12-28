9147310bfaf445760d6ff419d843a0acc90516a8
dreik84 <dreik84@mail.ru>
2025-12-28 10:37:31 +0300

    Create ValCurs.java

---
+++ src/main/java/org/example/ValCurs.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class ValCurs {
+    private LocalDate date;
+    private String name;
+    private List<Valute> valutes;
+}
