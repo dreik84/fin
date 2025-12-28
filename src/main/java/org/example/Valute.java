a831951d437b6063c4426b1e15bad30b31f7c366
dreik84 <dreik84@mail.ru>
2025-12-28 10:37:45 +0300

    Create Valute.java

---
+++ src/main/java/org/example/Valute.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Valute {
+    private String id;
+    private int numCode;
+    private String charCode;
+    private int nominal;
+    private String name;
+    private double value;
+    private double vunitRate;
+}
