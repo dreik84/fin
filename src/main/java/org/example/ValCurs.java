91790736dea9d830339b29cc7033b7b76dfa7e99
dreik84 <dreik84@mail.ru>
2025-12-29 11:15:41 +0300

    Update ValCurs.java

--- src/main/java/org/example/ValCurs.java
+++ src/main/java/org/example/ValCurs.java
@@ -1,10 +1,22 @@
 package org.example;
 
-import java.time.LocalDate;
+import lombok.Data;
+
+import javax.xml.bind.annotation.*;
 import java.util.List;
 
+@Data
+@XmlRootElement(name = "ValCurs")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class ValCurs {
-    private LocalDate date;
+
+    @XmlAttribute(name = "Date")
+    private String date;
+
+    @XmlAttribute(name = "name")
     private String name;
+
+    @XmlElementWrapper(name = "ValCurs")
+    @XmlElement(name = "Valute")
     private List<Valute> valutes;
 }
