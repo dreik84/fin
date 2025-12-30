8e1a2a269d76ccc8b66d965b3e5625ea41a963d6
dreik84 <dreik84@mail.ru>
2025-12-30 10:00:01 +0300

    Update Valute.java

--- src/main/java/org/example/Valute.java
+++ src/main/java/org/example/Valute.java
@@ -1,11 +1,34 @@
 package org.example;
 
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+
+@Data
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Valute {
+
+    @XmlAttribute(name = "ID")
     private String id;
+
+    @XmlElement(name = "NumCode")
     private int numCode;
+
+    @XmlElement(name = "CharCode")
     private String charCode;
+
+    @XmlElement(name = "Nominal")
     private int nominal;
+
+    @XmlElement(name = "Name")
     private String name;
+
+    @XmlElement(name = "Value")
     private double value;
+
+    @XmlElement(name = "VunitRate")
     private double vunitRate;
 }
