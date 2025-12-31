25163f652cd6fc6943baeae50f7d01d02dbff3ae
dreik84 <dreik84@mail.ru>
2025-12-31 10:31:44 +0300

    Update Valute.java

--- src/main/java/org/example/Valute.java
+++ src/main/java/org/example/Valute.java
@@ -4,31 +4,17 @@ import lombok.Data;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Valute {
-
-    @XmlAttribute(name = "ID")
-    private String id;
-
-    @XmlElement(name = "NumCode")
+    private Map<String, String> attributes = new HashMap<>();
     private int numCode;
-
-    @XmlElement(name = "CharCode")
     private String charCode;
-
-    @XmlElement(name = "Nominal")
     private int nominal;
-
-    @XmlElement(name = "Name")
     private String name;
-
-    @XmlElement(name = "Value")
     private double value;
-
-    @XmlElement(name = "VunitRate")
     private double vunitRate;
 }
