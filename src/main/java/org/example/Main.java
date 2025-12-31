a1826b0d523205245ea751f283e17e7f7798214c
dreik84 <dreik84@mail.ru>
2025-12-31 10:31:57 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,24 @@
 package org.example;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.io.StringReader;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException, JAXBException {
+    public static void main(String[] args) throws IOException, InterruptedException, ParserConfigurationException, SAXException {
         LocalDate today = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String currencyRate = Converter.getCurrencyByDate(formatter.format(today));
 
-        StringReader reader = new StringReader(currencyRate);
-        JAXBContext context = JAXBContext.newInstance(ValCurs.class);
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        ValCurs valCurs = (ValCurs) unmarshaller.unmarshal(reader);
+        ValCurs valCurs = new ValCurs();
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = builder.parse(currencyRate);
 
         System.out.println(currencyRate);
     }
